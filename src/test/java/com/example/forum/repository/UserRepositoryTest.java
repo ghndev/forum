d9312af89fea25f0b14d8ba550ae3791a4048ece
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2023-03-21 18:06:36 +0900

    test: UserRepository의 findByUsername 테스트

---
+++ src/test/java/com/example/forum/repository/UserRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.example.forum.repository;
+
+import com.example.forum.domain.Role;
+import com.example.forum.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User testUser;
+
+    @BeforeEach
+    void setUp() {
+        testUser = User.builder()
+                .email("test@test.com")
+                .username("testUser")
+                .password("password1")
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(testUser);
+    }
+
+    @Test
+    void findByUsername() {
+        User savedUser = userRepository.findByUsername(testUser.getUsername()).get();
+
+        assertThat(testUser).isEqualTo(savedUser);
+    }
+}
