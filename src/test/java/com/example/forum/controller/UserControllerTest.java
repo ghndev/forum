621e80bb1a88918fce1e60edddf153b8a1e156c1
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2023-03-28 23:05:27 +0900

    test: 로그인 테스트

--- src/test/java/com/example/forum/controller/UserControllerTest.java
+++ src/test/java/com/example/forum/controller/UserControllerTest.java
@@ -1,19 +1,19 @@
 package com.example.forum.controller;
 
 import com.example.forum.controller.dto.SignUpRequest;
+import com.example.forum.domain.Role;
 import com.example.forum.domain.User;
-import com.example.forum.repository.UserRepository;
-import org.junit.jupiter.api.DisplayName;
+import com.example.forum.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,7 +27,7 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Test
     void TestSignupForm() throws Exception {
@@ -65,4 +65,23 @@ class UserControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    void login() throws Exception {
+        String username = "loginTest";
+        String password = "password1";
+
+        User user = User.builder()
+                .username(username)
+                .password(password)
+                .email("test@test.com")
+                .role(Role.USER)
+                .build();
+
+        userService.createUser(user);
+
+        mockMvc.perform(formLogin().user(username).password(password))
+                .andExpect(authenticated())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
 }
